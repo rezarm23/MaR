56a0b82bf0ad1b4f1808dd9d6de270d0a62a5b79
rezarm23 <mohammadmansuri2123@gmail.com>
2025-05-02 15:29:37 +0330

    fix database settings for render

--- MaR_site/settings.py
+++ MaR_site/settings.py
@@ -13,6 +13,7 @@ import os
 from datetime import timedelta
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +84,11 @@ WSGI_APPLICATION = 'MaR_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
